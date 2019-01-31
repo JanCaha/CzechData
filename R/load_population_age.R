d8cddf3bb6e41f3de0f53be1e859bf05b1566dfc
Jan Caha <JanCaha@users.noreply.github.com>
2019-01-31 20:27:50 +0100

    added new sources of data about population

---
+++ R/load_population_age.R
@@ -0,0 +1,101 @@
+#' Load population by age
+#'
+#' Load population by sex and five-year age categories for years 2010 to 2017.
+#'
+#' @param year for which the data should be obtained. Default value is \code{NA}, which means
+#' all the years. Values from range (including both limits) 2010 - 2017 are accepted.
+#' @param area_type type of are for which the data should be obtained. Default value is \code{NA},
+#' which means all areas. Accepted values are in Description.
+#'
+#' @describeIn load_population_age Load the data
+#'
+#' @section Information about dataset:
+#'  More detailed information about data can be found at the provider's website
+#'  \url{https://www.czso.cz/csu/czso/obyvatelstvo-k-3112-podle-pohlavi-v-obcich}.
+#'
+#' @details
+#' The \code{area_type} can have values from following set:
+#' \enumerate{
+#'   \item \code{"okresy"}
+#'   \item \code{"kraje"}
+#'   \item \code{"republika"}
+#' }
+#'
+#' @return data.frame containg the requested data
+#' @export
+#'
+#' @importFrom glue glue glue_collapse
+#' @importFrom dplyr case_when mutate filter select
+#' @importFrom readr read_csv
+#' @importFrom lubridate year
+#'
+#' @examples
+#' population_age <- load_population_age(area_type = "okresy")
+load_population_age <- function(year = NA, area_type = NA){
+
+  area_types <- c("okresy", "kraje", "republika")
+
+  if (!is.na(area_type)) {
+    if (!area_type %in% area_types) {
+      stop(glue::glue("Unknow area_type - {area_type}. Allowed values are: ",
+                      glue::glue_collapse(area_types, sep = ", "), "."))
+    }
+  }
+
+  year_valid <- FALSE
+
+  if ( !is.na(year)) {
+    if (is.numeric(year)) {
+      if (2010 <= year & year <= 2017) {
+        year_valid <- TRUE
+      }else{
+        stop(glue::glue("Year has to be from range 2010 - 2017. ",
+                        "{year} does not fall in this range."))
+      }
+    }else{
+      stop(glue::glue("Variable year has to be numeric. It is {typeof(year)}."))
+    }
+  }
+
+  area_id <- dplyr::case_when(
+    area_type == "okresy" ~ 101,
+    area_type == "kraje" ~ 100,
+    area_type == "republika" ~ 97
+  )
+
+  data <- readr::read_csv("https://www.czso.cz/documents/62353418/83879838/130142-18data051818.csv",
+                          col_types = cols())
+
+  data <- data %>%
+    dplyr::mutate(rok = lubridate::year(casref_do))
+
+  if (!is.na(year_valid)) {
+    data <- data %>%
+      dplyr::filter(rok == year)
+  }
+
+  if (!is.na(area_type)) {
+    data <- data %>%
+      dplyr::filter(vuzemi_cis == area_id)
+  }
+
+  data %>%
+    dplyr::select(-rok)
+}
+
+#' @describeIn load_population_age Load description for columns
+#'
+#' @importFrom jsonlite fromJSON
+#'
+#'@export
+#'
+load_population_age_col_explanations <- function(){
+
+  json_file <- jsonlite::fromJSON("https://www.czso.cz/documents/62353418/83879838/130142-18schema051818.json")
+
+  data <- json_file$tableSchema$columns[,c(2,3)]
+
+  names(data) <- c("sloupec", "popis")
+
+  data
+}
---
+++ R/load_population_settlements.R
@@ -0,0 +1,82 @@
+#' Load populations from settlements
+#'
+#' Load population by sex for settlements for each years from 2000 to 2017.
+#'
+#' @param year year for which the data should be obtained. Default value is \code{NA}, which means
+#' all the years. Values from range (including both limits) 2000 - 2017 are accepted.
+#'
+#' @describeIn load_population_settlements Load the data
+#'
+#' @section Information about dataset:
+#'  More detailed information about data can be found at the provider's website
+#'  \url{https://www.czso.cz/csu/czso/obyvatelstvo-podle-petiletych-vekovych-skupin-a-pohlavi-v-krajich-a-okresech}.
+#'
+#' @return data.frame containg the requested data
+#' @export
+#'
+#' @importFrom utils download.file unzip
+#' @importFrom readr read_csv
+#' @importFrom glue glue
+#'
+#' @examples
+#' # population for all years
+#' population <- load_population_settlements()
+#'
+#' # population for year 2015
+#' population_2015 <- load_population_settlements(year = 2015)
+#'
+load_population_settlements <- function(year = NA){
+
+  year_valid <- FALSE
+
+  if ( !is.na(year)) {
+    if (is.numeric(year)) {
+      if (2000 <= year & year <= 2017) {
+       year_valid <- TRUE
+      }else{
+        stop(glue::glue("Year has to be from range 2000 - 2017. ",
+                        "{year} does not fall in this range."))
+      }
+    }else{
+      stop(glue::glue("Variable year has to be numeric. It is {typeof(year)}."))
+    }
+  }
+
+  temp_dir <- tempdir()
+
+  obyvatelstvo_file <- file.path(temp_dir, "obyvatelstvo_obce.zip")
+
+  if (!file.exists(obyvatelstvo_file)) {
+
+    utils::download.file("https://www.czso.cz/documents/62353418/74123173/130149-18data051818.zip",
+                         obyvatelstvo_file)
+
+  }
+
+  obyvatelstvo_file <- utils::unzip(obyvatelstvo_file, exdir = temp_dir)
+
+  obyvatelstvo <- readr::read_csv(obyvatelstvo_file, col_types = cols())
+
+  if (year_valid) {
+    obyvatelstvo <- obyvatelstvo %>%
+      filter(rok == year)
+  }
+
+  obyvatelstvo
+}
+
+#' @describeIn load_population_settlements Load description for columns
+#'
+#' @importFrom jsonlite fromJSON
+#'
+#'@export
+#'
+load_population_settlements_col_explanations <- function(){
+  json_file <- jsonlite::fromJSON("https://www.czso.cz/documents/62353418/74123173/130149-18schema051818.json")
+
+  data <- json_file$tableSchema$columns[,c(2,3)]
+
+  names(data) <- c("sloupec", "popis")
+
+  data
+}
