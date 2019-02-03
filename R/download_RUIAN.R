fa65bc55ec8c96a4004210ddf17e2c6f76ffbee3
Jan Caha <JanCaha@users.noreply.github.com>
2019-02-03 21:12:59 +0100

    styling the code

--- R/download_RUIAN.R
+++ R/download_RUIAN.R
@@ -1,8 +1,7 @@
 #' @importFrom utils download.file unzip
 # simple function that downloads large zipped RUIAN file, extracts it and returns the path to the
 # unzipped folder
-.download_RUIAN <- function(){
-
+.download_RUIAN <- function() {
   message("Downloading roughly 190 MB, this can take a while.")
 
   temp_dir <- tempdir()
@@ -10,9 +9,7 @@
   ruian_file <- file.path(temp_dir, "RUIAN.zip")
 
   if (!file.exists(ruian_file)) {
-
     utils::download.file("http://services.cuzk.cz/shp/stat/epsg-5514/1.zip", ruian_file)
-
   }
 
   utils::unzip(ruian_file, exdir = temp_dir)
