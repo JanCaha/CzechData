37ada0fdeb5a7cb5b03efdfeaef6c644abd142c7
Jan Caha <JanCaha@users.noreply.github.com>
2019-04-04 19:20:31 +0200

    fix message for RUIAN download show correctly

--- R/download_RUIAN.R
+++ R/download_RUIAN.R
@@ -1,14 +1,16 @@
 #' @importFrom utils download.file unzip
+#'
 # simple function that downloads large zipped RUIAN file, extracts it and returns the path to the
+#
 # unzipped folder
 .download_RUIAN <- function() {
-  message("Downloading roughly 190 MB, this can take a while.")
 
   temp_dir <- tempdir()
 
   ruian_file <- file.path(temp_dir, "RUIAN.zip")
 
   if (!file.exists(ruian_file)) {
+    message("Downloading roughly 190 MB, this can take a while.")
     utils::download.file("http://services.cuzk.cz/shp/stat/epsg-5514/1.zip",
                          ruian_file,
                          quiet = TRUE)
