e14525892ad00027351a331ff23699a954d2a9fb
Jan Caha <jan.caha@outlook.com>
2021-01-13 23:38:47 +0100

    add tests that check that all downloaded layers are listed and can be acessed

--- tests/testthat/test-load_RUIAN.R
+++ tests/testthat/test-load_RUIAN.R
@@ -26,3 +26,16 @@ test_that("Load cadastral data - wrong id", {
   expect_error(load_RUIAN_settlement("3722910", "adresni mista", "Variable id must follow the general pattern"))
   expect_error(load_RUIAN_settlement("599999", "adresni mista", "There is no settlement in Czech Republic"))
 })
+
+test_that("Testing that for random ids all layers are listed in package function `.settlement_RUIAN_layers()`", {
+  for (id in ids) {
+    files <- list.files(file.path(tempdir(), "534048"), "shp")
+    files <- stringr::str_remove(files, "\\.shp")
+
+    for (file in files){
+
+      expect_true(file %in% .settlement_RUIAN_layers()$alias)
+    }
+  }
+})
+
